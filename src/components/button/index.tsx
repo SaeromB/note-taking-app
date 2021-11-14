532053dbbf9a5e48e5e37edd476b6b9f40c2672a
Saerom Bang <saerombang11@gmail.com>
2021-11-14 22:06:33 +0900

    Changed button style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 const Button: FC = () => {
   return (
     <button className="bg-gray-600 bg-opacity-100 rounded-md">
-      <div className="text-white text-xs leading-5 px-md py-sm">Save</div>
+      <div className="text-white text-xs leading-5 py-2 px-4">Save</div>
     </button>
   )
 }
