9380429bdec7df8dcec76fb32fd790b6326a1070
Saerom Bang <saerombang11@gmail.com>
2021-12-10 17:15:24 +0900

    Added ManageNote modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,27 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Button from '../src/components/button'
 import Card from '../src/components/card'
-import CreateNote from '../src/components/create-note'
-import { useState } from 'react'
+import CreateNoteModal from '../src/components/create-note'
+import ManageNote from '../src/components/manage-note'
+
+import React, { useState } from 'react'
 
 const Home = () => {
-  const [showCreateModal, setCreateModal] = useState(false)
-  const onClick = () => setCreateModal(true)
+  const [show, setShow] = useState(false)
 
   return (
     <>
-      {showCreateModal ? <CreateNote /> : null}
       <div className="p-4">
         <div className="flex justify-between">
           <h1 className="font-semibold	leading-7	text-xl">Note Taking</h1>
-          <button onClick={onClick}>Create</button>
+          <button onClick={() => setShow(true)}>Create</button>
+          {/* <CreateNoteModal show={show} setShow={setShow}>
+            This is inside the modal!
+          </CreateNoteModal> */}
+          {/* <button onClick={() => setShow(true)}>Create</button>
+          <CreateNoteModal>Hello</CreateNoteModal> */}
         </div>
+        <ManageNote />
         <div className="grid grid-cols-2 mt-4 gap-4 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6">
           <Card />
           <Card />
@@ -28,6 +34,7 @@ const Home = () => {
           <Card />
           <Card />
         </div>
+        <CreateNoteModal show={show} setShow={setShow}></CreateNoteModal>
       </div>
     </>
   )
--- src/components/create-note/index.tsx
+++ src/components/create-note/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const CreateNote = () => {
-  return <div className="fixed">Modal</div>
+type CreateModalProps = {
+  closeable?: boolean
+  show?: any
+  setShow?: any
 }
 
-export default CreateNote
+// const CreateNoteModal: FC<CreateModalProps> = ({ children, show, setShow }) => {
+//   return (
+//     show && (
+//       <div className="fixed z-10 inset-0 overflow-y-auto bg-gray-500 opacity-75">
+//         <div className="bg-white m-4 p-6 rounded-lg">
+//           <div className="flex items-center justify-between">
+//             <div className="text-lg">Create Note</div>
+//             <button onClick={() => setShow(false)}>X</button>
+//           </div>
+//           <div className="mt-6">
+//             <div>Title</div>
+//             <input />
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   )
+// }
+
+const CreateNoteModal: FC<CreateModalProps> = ({ children, show, setShow }) => {
+  return (
+    show && (
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+            <div className="inline-block w-full align-bottom bg-white rounded-md text-gray-500">
+              <div className="flex items-center justify-between">
+                <div className="text-lg">Create Note</div>
+                <button onClick={() => setShow(false)}>X</button>
+              </div>
+              <div>
+                <div className="text-left">Title</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  )
+}
+
+export default CreateNoteModal
