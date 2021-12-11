e4a2d5c6213feecb6f082ad9afbbfd4fb1f50463
Saerom Bang <saerombang11@gmail.com>
2021-12-11 20:54:54 +0900

    Change CreateNoteModal style

--- src/components/create-note/index.tsx
+++ src/components/create-note/index.tsx
@@ -6,40 +6,34 @@ type CreateModalProps = {
   setShow?: any
 }
 
-// const CreateNoteModal: FC<CreateModalProps> = ({ children, show, setShow }) => {
-//   return (
-//     show && (
-//       <div className="fixed z-10 inset-0 overflow-y-auto bg-gray-500 opacity-75">
-//         <div className="bg-white m-4 p-6 rounded-lg">
-//           <div className="flex items-center justify-between">
-//             <div className="text-lg">Create Note</div>
-//             <button onClick={() => setShow(false)}>X</button>
-//           </div>
-//           <div className="mt-6">
-//             <div>Title</div>
-//             <input />
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   )
-// }
-
 const CreateNoteModal: FC<CreateModalProps> = ({ children, show, setShow }) => {
   return (
     show && (
       <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
-            <div className="inline-block w-full align-bottom bg-white rounded-md text-gray-500">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center bg-gray-500 bg-opacity-75 ">
+          {/* Modal Content*/}
+          <div className="block w-full align-bottom bg-white p-6 rounded-md">
+            {/* Modal Head */}
+            <form>
               <div className="flex items-center justify-between">
-                <div className="text-lg">Create Note</div>
+                <h3 className="text-lg">Create Note</h3>
                 <button onClick={() => setShow(false)}>X</button>
               </div>
-              <div>
-                <div className="text-left">Title</div>
+
+              <div className="mt-6 text-left">
+                <label className="">Title</label>
               </div>
-            </div>
+              <div className="mt-1">
+                <input className="w-full border border-gray-300 rounded-md"></input>
+              </div>
+
+              <div className="mt-6 text-left">
+                <label className="">Content</label>
+              </div>
+              <div className="mt-1">
+                <input className="w-full border border-gray-300 rounded-md"></input>
+              </div>
+            </form>
           </div>
         </div>
       </div>
